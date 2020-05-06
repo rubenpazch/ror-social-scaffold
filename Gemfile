026a251cfbf4be2f6bdab3aa571913aa74fdbea4
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-05-06 18:01:19 -0500

    validations with rubocop

--- Gemfile
+++ Gemfile
@@ -41,13 +41,13 @@ gem 'devise'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
-  gem 'rspec-rails', '~> 4.0.0'
   gem 'database_cleaner'
+  gem 'rspec-rails', '~> 4.0.0'
 end
 
 group :test do
-  gem 'rspec'
   gem 'capybara', '>= 2.15'
+  gem 'rspec'
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Friendship, type: :model do
   context 'validation tests' do
     before :each do
-      #@user = users(:first)
+      # @user = users(:first)
       @friendship = Friendship.new(user_id: 1, friend_id: 2, status: 0)
     end
 
@@ -15,17 +15,14 @@ RSpec.describe Friendship, type: :model do
       @friendship.status = '   '
       expect(@friendship).to_not be_valid
     end
-    
   end
 
   context 'association tests' do
     describe 'friendship model associations' do
-      
       it 'belongs to user' do
         assc = Friendship.reflect_on_association(:user)
         expect(assc.macro).to eq :belongs_to
       end
-      
     end
   end
 end
