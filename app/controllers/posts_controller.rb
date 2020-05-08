1041808dcd9cc101ce2fa8b31031a1dc03c5d863
idgm5 <idgm@pm.me>
2020-05-08 15:30:53 -0500

    Recent posts by friends and current user for timeline

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,27 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    @invitations = current_user.invites
+    @invitations.each do |fblock|
+      @friendId = fblock.user_id
+      @friend = User.find(@friendId)
+      @friend_posts_invites ||= @friend.posts.ordered_by_most_recent
+    end
+
+    @friendships = current_user.friendships
+    @friendships.each do |fblock|
+      @friendId = fblock.friend_id
+      @friend = User.find(@friendId)
+      @friend_posts_friendships ||= @friend.posts.ordered_by_most_recent
+    end
+    
+    if @friend_posts_invites.nil?
+      @timeline_friend_posts ||= @friend_posts_friendships.all.ordered_by_most_recent
+    else
+      @timeline_friend_posts ||= @friend_posts_invites.all.ordered_by_most_recent
+    end
+
+    @timeline_posts ||= current_user.posts.all.ordered_by_most_recent
   end
 
   def post_params
