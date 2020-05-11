ba02050a52dc12e0ecf083597b2995561fae05a7
idgm5 <idgm@pm.me>
2020-05-11 16:56:30 -0500

    Add 'addfriend' tests for usersControllers and WIP 'accept' for friendshipsController

--- spec/controllers/friendship_spec.rb
+++ spec/controllers/friendship_spec.rb
@@ -2,19 +2,39 @@ require 'rails_helper'
 
 RSpec.describe FriendshipsController, type: :controller do
     let(:user) { FactoryBot.create :user }
+    let(:friend) { FactoryBot.create :friend }
+    let(:friendtwo) { FactoryBot.create :friendtwo }
+
     before { sign_in user }
-    
-    describe '#new' do
-        it 'should initialize empty friendship' do
-          get :new, params: { id: user.id }
-          expect(user.friendships.empty?).to eql(true)
-        end
-    end
+    before { sign_in friend }
+    before { sign_in friendtwo }
 
     describe '#accept' do
-      it 'should initialize empty friendship index' do
-        post :accept, params: { id: user.id }
-        expect(user.friendships.empty?).to eql(true)
+      it 'should update a friendship object' do
+        expect {
+          post :addfriend, params: {user_id: user, friend_id: friend, status: 0}
+        }.to change(Friendship, :count).by(1)
+      end
+
+      it 'redirects to root_path after new object' do
+         post :addfriend, params: {user_id: user, friend_id: friendtwo, status: 0}
+         expect(response).to redirect_to '/'
       end
     end
+    # describe '#new' do
+    #     it 'should initialize empty friendship' do
+    #       get :new, params: { id: user.id }
+    #       expect(user.friendships.empty?).to eql(true)
+    #     end
+    # end
+    #
+    # describe '#accept' do
+    #   it 'should update status to 1' do
+    #       @user1 = User.first
+    #       @user2 = User.second
+    #       @one = Friendship.create(user_id: @user1, friend_id: @user2, status: 0)
+    #       put :accept, :id => @one.id, :friendship => @one.attributes = { :status => 1 }
+    #       response.should be_successful
+    #   end
+    # end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,4 +5,18 @@ FactoryBot.define do
       password { 'password' }
       password_confirmation { 'password' }
     end
+
+    factory :friend, class: 'User' do
+      name { 'Perez' }
+      email { 'perez@gmail.com' }
+      password { 'password' }
+      password_confirmation { 'password' }
+    end
+
+    factory :friendtwo, class: 'User' do
+      name { 'David' }
+      email { 'david@gmail.com' }
+      password { 'password' }
+      password_confirmation { 'password' }
+    end
   end
