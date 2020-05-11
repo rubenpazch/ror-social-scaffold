b01f288ae4e1bc681c07e631fb829eed70d34ffc
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-05-11 18:02:38 -0500

    add some wp accept invitation

--- spec/controllers/friendships_controller_spec.rb
+++ spec/controllers/friendships_controller_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe FriendshipsController, type: :controller do
   let(:user) { FactoryBot.create :user }
-  let(:friendship) { FactoryBot.create :friendship }
+  let(:user2) { FactoryBot.create :user }
+  let(:friendship1) { FactoryBot.create :friendship }
+  
   before { sign_in user }
+  before { sign_in user2 }  
 
   describe '#new' do
     it 'should initialize empty friendship' do
@@ -16,6 +19,12 @@ RSpec.describe FriendshipsController, type: :controller do
     it 'should initialize empty friendship index' do
       put :accept, params: { status: 1 }
       expect(friendships.empty?).to eql(true)
+    end
+
+    it "redirects to root_url" do    
+      
+      put :accept, params: { :user_id => friendship1.user_id, :friend_id => friendship1.friend_id, :status =>1 } 
+      expect(friendship1.reload.status).to  eql(1)
     end
   end
 end
